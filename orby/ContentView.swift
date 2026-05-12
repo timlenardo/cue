900152540d1896793cd48fd5cbe1ae10eee823c5
timl <tim.lenardo@gmail.com>
2026-05-11 18:40:56 -0700

    Initial Commit

---
+++ orby/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  orby
+//
+//  Created by Timothy Lenardo on 5/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ orby/orbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  orbyApp.swift
+//  orby
+//
+//  Created by Timothy Lenardo on 5/11/26.
+//
+
+import SwiftUI
+
+@main
+struct orbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
